c7dbd3817ccac3c177adfb4adaee89b2f717df12
Flynnt-Dev <81318052+Flynnt-Dev@users.noreply.github.com>
2021-04-04 18:53:33 +0200

    HooksPreparation of BlogSection.js

--- src/BlogSection.js
+++ src/BlogSection.js
@@ -1,11 +1,47 @@
 import React from "react";
 import PostList from "./PostList";
 
+/*
+function BlogSection(){
+  const [posts, setPosts]=React.setState();
+  const [loading, setloading]=React.setState(true);
+
+  React.useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/posts/")
+          .then((res) => res.json())
+          .then((posts) => setPosts(posts))
+          .finally((laoding) => setLoading(!loading));
+
+      return()=>{
+        console.log("Bye!");
+      };
+    },
+    []
+  );
+
+  return(
+    <div className="App">
+        <h1>Welcome, {this.state.user}</h1>
+        <h2>Here are you favorite articles</h2>
+        {loading ? (
+          <h1>loading posts</h1>
+        ) : (
+          <PostList posts={posts} />
+        )}
+
+        <button onClick={this.state.onLogout()}>Logout</button>
+    </div>
+  )
+}
+
+
+*/
 class BlogSection extends React.Component {
   state = {
     posts: [],
     loading: true
   };
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/posts/")
       .then((res) => res.json())
